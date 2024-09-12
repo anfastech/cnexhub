4cb9098c10e1930ead8f36f8f1bc818b8b323f03
Warrior <kpanfas6@gmail.com>
2024-09-12 23:42:38 +0530

    all new changes: nextjs

---
+++ app/components/Hello.tsx
@@ -0,0 +1,17 @@
+
+// export default function Hello() {
+//     return (
+//         <h1 style={{fontSize: "40px"}}>Hello Worlds..</h1>
+//     )
+// }
+
+
+
+export default function Hello() {
+    
+    return (
+        <div>
+          <h1>Hello Anfas Boss</h1>
+        </div>
+    );
+};
